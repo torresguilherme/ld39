790ac2cf5b7d84489b84be0f51b485b5ac3fc39a
Guilherme Torres <guilhermetorres97@gmail.com>
2017-07-31 16:36:57 -0300

    Colisão com o teto, level em progresso

---
+++ projeto/scripts/player/head.gd
@@ -0,0 +1,16 @@
+extends RayCast2D
+
+onready var player = get_node("../").get_node("../")
+var body
+
+func _ready():
+	add_exception(player)
+	set_process(true)
+	pass
+
+func _process(delta):
+	if is_colliding():
+		body = get_collider()
+		if body:
+			if body.is_in_group(global.GROUND_GROUP) || body.is_in_group(global.ENEMY_GROUP):
+				player.y_velocity = 0
